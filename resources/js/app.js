fcd1038b4a35b8ce32f73217ac8be500092b1122
MarcossDiazPerez <diazperezmarcoss@gmail.com>
2020-02-13 21:48:43 -0600

    laravel + vue + vuetify completo

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,11 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuetify from "../plugins/vuetify"
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
-    el: '#app',
+    vuetify: Vuetify,
+    el: '#app'
 });
