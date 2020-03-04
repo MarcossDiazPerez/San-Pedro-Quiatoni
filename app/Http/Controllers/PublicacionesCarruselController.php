3f72992fcd9c952ed0636bd9e2840eb62ffdcc8b
kevin <velasquezruizkevin@gmail.com>
2020-03-04 10:18:16 -0600

    migracion, prueba push

---
+++ app/Http/Controllers/PublicacionesCarruselController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\publicacionesCarrusel;
+use Illuminate\Http\Request;
+
+class PublicacionesCarruselController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\publicacionesCarrusel  $publicacionesCarrusel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(publicacionesCarrusel $publicacionesCarrusel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\publicacionesCarrusel  $publicacionesCarrusel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(publicacionesCarrusel $publicacionesCarrusel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\publicacionesCarrusel  $publicacionesCarrusel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, publicacionesCarrusel $publicacionesCarrusel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\publicacionesCarrusel  $publicacionesCarrusel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(publicacionesCarrusel $publicacionesCarrusel)
+    {
+        //
+    }
+}
